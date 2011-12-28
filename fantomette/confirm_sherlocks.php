4ec4331133cf863bfdd966d1a057e929c3f52de8
Sid3y1 <Sid3y1>
2011-12-28 16:08:04 +0100

    Deplacement de fantomette dans le repertoire fantomette standalone

--- fantomette/confirm_sherlocks.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include_once('config.inc.php');
-//TODO: changer ca ! 
-//$client = new SoapClient("https://10.10.10.1:4430/SBUY.class.php?wsdl");
-$client->transactionDecode(base64_encode($_POST['DATA']));
-
-?>
