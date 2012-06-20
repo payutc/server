dfa0e83d07157c93a6030d4f5387002a6b0cd679
Arthur Puyou <arthur@puyou.fr>
2012-06-20 02:34:18 +0200

    Simplification et gestion de plus de cas limites

--- class/Cas.class.php
+++ class/Cas.class.php
@@ -8,17 +8,14 @@ class Cas {
 	public static function authenticate($ticket,$service) {
 		global $_CONFIG;
 		$url_validate = $_CONFIG['cas_url']."serviceValidate?service=".$service."&ticket=".$ticket;
-		$get_reponse = fopen($url_validate, "r");
-		$data=''; 
-		while(!feof($get_reponse)) 
-			$data.=fread($get_reponse,100); 
-		fclose($handle);
+		$data = file_get_contents($url_validate);
+		if(empty($data)) return -1;
+		
 		$parsed = new xmlToArrayParser($data);
 		if(isset($parsed->array['cas:serviceResponse']['cas:authenticationSuccess']['cas:user'])) 
-			$login = $parsed->array['cas:serviceResponse']['cas:authenticationSuccess']['cas:user']; 
+			return $parsed->array['cas:serviceResponse']['cas:authenticationSuccess']['cas:user']; 
 		else
-			$login = -1;
-		return $login;
+			return -1;
 	}
 	
 	public static function getURl() {
