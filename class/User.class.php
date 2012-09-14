ef316df3fbf3328e5fac356b4b1f52d6fc7e5b11
Matthieu Guffroy <mattgu74@gmail.com>
2012-09-14 16:36:44 +0200

    annulation

--- class/User.class.php
+++ class/User.class.php
@@ -553,7 +553,7 @@ class User {
 	* @return array $return
 	*/
 	public function getLastPurchase() {
-		$res = Db_buckutt::getInstance()->query("SELECT pur_id, obj_id, pur_price FROM t_purchase_pur WHERE UNIX_TIMESTAMP(pur_date) > (UNIX_TIMESTAMP(NOW()) - 900) AND usr_id_buyer = %u",array($this->idUser));
+		$res = Db_buckutt::getInstance()->query("SELECT pur_id, obj_id, pur_price FROM t_purchase_pur WHERE UNIX_TIMESTAMP(pur_date) > (UNIX_TIMESTAMP(NOW()) - 900) AND usr_id_buyer = %u AND pur_removed = 0",array($this->idUser));
 		$pur = array();
 		while ($don = Db_buckutt::getInstance()->fetchArray($res)) { $pur[$don['pur_id']] = $don;}
 		return $pur;
