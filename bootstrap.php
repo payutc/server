c5b951e918180d8dd740ed433deee0a1fe83d1c0
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-11-14 01:08:35 +0100

    bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,8 @@
 <?php
-// Include the main Propel script, initialize it and add classes to inc path
-require_once 'lib/propel/runtime/lib/Propel.php';
+// Include all dependencies
+require_once 'vendor/autoload.php';
+
+// Include model
 Propel::init('propel/build/conf/payutc-conf.php');
 set_include_path('propel/build/classes' . PATH_SEPARATOR . get_include_path());
 
