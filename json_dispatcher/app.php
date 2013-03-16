8bc8a012c9b85287a3e856ef22e37a80f841e32a
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2013-03-16 17:37:50 +0100

    allow multiple services in session

--- json_dispatcher/app.php
+++ json_dispatcher/app.php
@@ -67,13 +67,12 @@ function handler($services, $service, $method)
 	$app->contentType('application/json; charset=utf-8');
 	if (array_key_exists($service, $services)) {
 		require_once $services[$service];
-		if (!isset($_SESSION[$service])) {
+		if (!array_key_exists($service, $_SESSION['services'])) {
 			$obj = new $service;
 		}
 		else {
-			$obj = unserialize($_SESSION[$service]);
+			$obj = unserialize($_SESSION['services'][$service]);
 		}
-		//var_dump($obj);
 		$a = call_user_func_named(array($obj, $method), $_REQUEST);
 		$_SESSION[$service] = serialize($obj);
 		echo json_encode($a);
