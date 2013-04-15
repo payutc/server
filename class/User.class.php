b4f363cbfcd72dc1e373fbd722e6907719258def
Arthur Puyou <arthur@puyou.fr>
2013-04-15 20:41:19 +0200

    Amélioration de la classe User

--- class/User.class.php
+++ class/User.class.php
@@ -32,6 +32,8 @@
 
 use \Payutc\Exception\UserIsBlockedException;
 use \Payutc\Exception\MessageUpdateFailedException;
+use \Payutc\Exception\UserUnknown;
+use \Payutc\Exception\GingerFailure;
 use \Payutc\Bom\Blocked;
 use \Payutc\Config;
 use \Payutc\Bom\MsgPerso;
@@ -43,20 +45,14 @@ use \Payutc\Db;
 class User {
 
 	protected $idUser;
-	protected $state = 401;
-	protected $groups;
-	protected $roles;
 	protected $lastname;
 	protected $firstname;
 	protected $nickname;
 	protected $mail;
 	protected $credit;
-	protected $ip;
-	protected $logWithPass = 0; //1 si le user s'est connecté avec mot de passe
 	protected $idPhoto;
 	protected $usr_fail_auth;
 	protected $max_fail_auth = 3; // Nombre max de mauvaises authentifications successives
-	protected $droits = Array();
 	protected $db;
 	protected $adult;
 	protected $msg_perso; // petit message imprimé en bas du ticket
@@ -66,55 +62,26 @@ class User {
 	/**
 	* Constructeur
 	* 
-	* @param string $data
-	* @param int $meanOfLogin
-	* @param string $pass
-	* @param string $ip[optional]
-	* @param int $noPass[optional] 1 si connexion sans mot de passe (badgeage simple au foyer)
-	* @param int $checkBlocked[optional] 0 si le fait d'être bloqué n'empéche pas la connexion (lorsqu'une carte est bloqué c'est pour payer on peut toujours accéder à casper/fantomette)
-	* @return int $state
+	* @param string $username Login of the User object to init
+	* @param int $checkBlocked True if a blocked user should throw an exception
 	*/
-	public function __construct($data, $meanOfLogin, $pass,  $ip = 0, $noPass = 0, $checkBlocked = 1) {
+	public function __construct($username, $checkBlocked = true) {
 		$this->db = Db_buckutt::getInstance();
-		
-		//Test si on utilise le mean of login IDCarte et si oui ba on suprime le dernier chiffre
-		//On fait ça parce qu'on ne sait pas le calculer 
-		if ($meanOfLogin == 4){
-			$data=substr($data, 0, -1);
-		}
-
+	
 		//Récupération de l'id buckutt
-		$this->idUser = $this->db->result($this->db->query("SELECT usr_id FROM tj_usr_mol_jum WHERE jum_data='%s' AND mol_id='%u';", Array($data, $meanOfLogin)),0);
-		if ($this->db->affectedRows() == 1) {
-			$this->state = 1;
-		} else {
-			$this->state = 405;
-			return $this->state;
+        $userQuery = $this->db->query("SELECT usr_id, usr_fail_auth, usr_blocked, usr_firstname, usr_lastname, usr_nickname, usr_adult, usr_msg_perso, usr_mail, usr_credit, img_id FROM ts_user_usr WHERE usr_nickname = '%s'", array($username));
+		if ($this->db->affectedRows() != 1) {
+			throw new UserUnknown();
 		}
-		
-		$don = $this->db->fetchArray($this->db->query("SELECT usr_fail_auth, usr_blocked FROM ts_user_usr WHERE usr_id = '%u';", Array($this->idUser)));
-		if ($this->db->affectedRows() == 1) {
-			$this->usr_fail_auth = $don['usr_fail_auth'];
-			//si utilisateur bloqué
-			if ($don['usr_blocked'] == 1 && $checkBlocked == 1) {
-				$this->state = 403;
-				return $this->state;
-			}
-		} else {
-			$this->state = 405;
-			return $this->state;
-		}		
-		
-		//si mec loggué avec mot de passe
-		if($noPass == 0) {
-			//si le mot de passe est faux, on sort
-			if ($this->checkPass($pass) != 1)
-				return $this->state;
-		}
+		$don = $this->db->fetchArray($userQuery);
         
-        //si on arrive jusque là, on peut charger le mec
-		$this->Ip = $ip;
-		$don = $this->db->fetchArray($this->db->query("SELECT usr_firstname, usr_lastname, usr_nickname, usr_adult, usr_mail, usr_credit, img_id FROM ts_user_usr WHERE usr_id = '%u';", Array($this->idUser)));		
+        $this->usr_fail_auth = $don['usr_fail_auth'];
+		//si utilisateur bloqué
+		if ($don['usr_blocked'] == 1 && $checkBlocked == 1) {
+            throw new UserBlocked();
+		}
+        		
+		//si on arrive jusque là, on peut charger le mec
 		$this->lastname = $don['usr_lastname'];
 		$this->firstname = $don['usr_firstname'];
 		$this->nickname = $don['usr_nickname'];
@@ -123,45 +90,6 @@ class User {
 		$this->idPhoto = $don['img_id'];
 		$this->adult = $don['usr_adult'];
 		$this->msg_perso = $this->getMsgPerso($this->idUser);
-			
-		$this->loadRights();
-	}
-
-	/**
-	* Vérifie si le mot de passe est correct
-	* 
-	* @param String $pass
-	* @return int $state
-	*/
-	public function checkPass($pass) {
-		//Vérification du couple id/pass
-		if ($this->db->numRows($this->db->query("SELECT usr_id FROM ts_user_usr WHERE usr_id='%u' AND usr_pwd='%s'", array($this->idUser, md5($pass)))) == 1) {
-			$this->logWithPass = 1;
-			//Si le usr_fail_auth n'est pas à 0, on le met à 0		
-			//if ($this->usr_fail_auth < 0)
-				$this->db->query("UPDATE ts_user_usr SET usr_fail_auth=0 WHERE usr_id='%u'", array($this->idUser));
-			$this->state = 1;
-		}
-		//sinon, on incrémente le usr_fail_auth
-		else {
-			if ($this->usr_fail_auth < $this->max_fail_auth) {
-				$this->db->query("UPDATE ts_user_usr SET usr_fail_auth=usr_fail_auth+1 WHERE usr_id='%u'", array($this->idUser));
-				$this->state = 406;
-			} else {
-				$this->db->query("UPDATE ts_user_usr SET usr_fail_auth=0, usr_blocked='1' WHERE usr_id='%u'", array($this->idUser));
-				$this->state = 403;
-			}
-		}
-		return $this->state;
-	}
-	
-	/**
-	* Retourne $state.
-	* 
-	* @return int $state
-	*/
-	public function getState() {
-		return $this->state;
 	}
 
 	/**
@@ -215,11 +143,15 @@ class User {
 	* @return int $credit
 	*/
 	public function getCredit() {
-		$this->credit = $this->db->result($this->db->query("SELECT usr_credit FROM ts_user_usr WHERE usr_id = %u",array($this->idUser)),0);
-		if ($this->db->affectedRows() == 1)
-			return $this->credit;
-		else
-			return 400;
+        $creditQuery = $this->db->query("SELECT usr_credit FROM ts_user_usr WHERE usr_id = %u", array($this->idUser));
+        
+		$this->credit = $this->db->result($creditQuery, 0);
+        
+		if ($this->db->affectedRows() == 1) {
+		    return $this->credit;
+		} else {
+		    throw new UserUnknown();
+		}
 	}
 	
 	/**
@@ -229,24 +161,6 @@ class User {
 	*/
 	public function getIdPhoto() {
 		return $this->idPhoto;
-	}
-
-	/**
-	* Retourne $ip.
-	* 
-	* @return string $ip
-	*/
-	public function getIp() {
-		return $this->ip;
-	}
-	
-	/**
-	* Retourne $droits.
-	* 
-	* @return array $droits
-	*/
-	public function getDroits() {
-		return $this->droits;
 	}
 
     /**
@@ -298,21 +212,6 @@ class User {
 		} else
 			return 400;
 	}
-	
-	/**
-	* Change le pin
-	* 
-	* @param String $oldKey
-	* @param String $newKey
-	* @return int $state
-	*/
-	public function setPass($oldKey, $newKey) {
-		$this->db->query("UPDATE ts_user_usr SET usr_pwd = '%s' WHERE usr_pwd = '%s' AND usr_id ='%u'", array(md5($newKey), md5($oldKey), $this->idUser));
-		if ($this->db->affectedRows() == 1) {
-			return 1;
-		} else
-			return 400;
-	}	
 
 	/**
 	* Fonction pour se bloquer soi même (en cas de perte/vol par exemple)
@@ -383,7 +282,7 @@ class User {
 	 */
 	public function checkNotBlockedMe() {
 		if ($this->isBlockedMe()) {
-			throw new UserIsBlockedException("L'utilisateur s'est auto bloqué.");
+			throw new GingerFailure("L'utilisateur s'est auto bloqué.");
 		}
 	}
 	
@@ -418,192 +317,15 @@ class User {
 	* @return array $identity
 	*/
 	public function getIdentity() {
-		if($this->db->numRows($this->db->query("SELECT jug.grp_id As grp_id FROM tj_usr_grp_jug jug, t_period_per per WHERE jug.usr_id = '%u' AND jug.grp_id = '1' AND per.per_id = jug.per_id AND jug.jug_removed = '0' AND per.per_date_start <= NOW() AND per.per_date_end >= NOW();",array($this->idUser))) > 0) {
-			$ajout = "BDE ";
-		} else{
-			$ajout= "/!\ pas BDE /!\ ";
-		}
-		return array($this->idUser, $this->getFirstname(), $this->getLastname(), $this->getNickname(), $this->getIdPhoto(), $this->credit);
+		return array(
+		    "id" => $this->idUser,
+            "firstname" => $this->getFirstname(),
+            "lastname" => $this->getLastname(),
+            "nickname" => $this->getNickname(),
+            "photo" => $this->getIdPhoto(),
+            "credit" => $this->credit);
 	}
 	
-	/**
-	* Charge les droits du user
-	* 
-	* @return int $state
-	*/	
-	public function loadRights() {
-		//on charge ses droits
-		$res = $this->db->query("SELECT rig.rig_id As rig_id, rig.rig_name As rig_name, rig.rig_admin As rig_admin, jur.fun_id As fun_id, jur.poi_id As poi_id, fun_name FROM ts_right_rig rig, t_period_per per, tj_usr_rig_jur jur LEFT JOIN t_fundation_fun fun ON fun.fun_id = jur.fun_id WHERE jur.usr_id = '%u' AND rig.rig_id = jur.rig_id AND per.per_id = jur.per_id AND jur.jur_removed = '0' AND rig.rig_removed = '0' AND per.per_date_start <= NOW() AND per.per_date_end >= NOW()",array($this->idUser));
-		if ($this->db->affectedRows() >= 1) {
-			$i = 0;
-			while ($don = $this->db->fetchArray($res)) {
-				$this->droits[$i]['rig_id'] = $don['rig_id'];
-				$this->droits[$i]['rig_name'] = $don['rig_name'];
-				$this->droits[$i]['fun_id'] = $don['fun_id'];
-				$this->droits[$i]['fun_name'] = $don['fun_name'];
-				$this->droits[$i]['poi_id'] = $don['poi_id'];
-				$this->droits[$i]['rig_admin'] = $don['rig_admin'];
-				$i++;
-			}
-			return 1;
-		} else {
-			return 400;
-		}
-	}
-	
-	/**
-	* on donne le droit (string genre "seller") avec eventuellement ce qui s'applique (id_fundation & id_point) et ça nous donne si le mec a le droit (1) ou pas (2) ou encore si on s'est planté dans le droit (452)
-	* 
-	* @param string $rig_name
-	* @param int $fun_id[optional]
-	* @param int $poi_id[optional]
-	* @return int $state
-	*/
-	public function hasDroit($rig_name, $fun_id = 0, $poi_id = 0) {
-		$return = 0;
-		foreach ($this->droits as $key => $value) {
-			
-			if(in_array($rig_name, $value))
-			{
-				if (($fun_id != 0) && ($poi_id != 0)) {
-					if ($value['fun_id'] == $fun_id AND $value['poi_id'] == $poi_id)
-						$return = 1;
-				} elseif ($fun_id != 0) {
-					if ($value['fun_id'] == $fun_id)
-						$return = 1;
-				} elseif ($poi_id != 0) {
-					if ($value['poi_id'] == $poi_id)
-						$return = 1;
-				} else {
-					$return = 1;
-				}
-			}
-		}
-		return $return;
-	}
-	
-	/**
-	 * Retourne si le user est seller
-	 * 
-	 * @param int $poi_id
-	 * @return int $state
-	 */
-	public function isSeller($poi_id = 0) {
-		return $this->hasDroit("seller", 0, $poi_id);
-	}
-	
-	/**
-	* Retourne si oui ou non l'user est admin d'une partie de buckutt
-	* 
-	* @return int $state
-	*/
-	public function isAdminBuckutt() {
-		if ($this->isBloqueur() == 1 || $this->isDroitAdmin() == 1 || $this->isBuckuttTrezo() == 1 || $this->isPointAdmin() == 1 || $this->isRespFundations() == 1)
-			return 1;
-		else
-			return 456;
-	}
-	
-	/**
-	 * Retourne si le user est bloqueur
-	 * 
-	 * @return int $state
-	 */
-	public function isBloqueur() {
-		if ($this->hasDroit("bloqueur",0,0) == 1)
-			return 1;
-		else
-			return 454;
-	}
-	
-	/**
-	 * Retourne si le user est droit_admin
-	 * 
-	 * @return int $state
-	 */
-	public function isDroitAdmin() {
-		if ($this->hasDroit("droit_admin",0,0) == 1)
-			return 1;
-		else
-			return 454;
-	}
-
-	/**
-	 * Retourne si le user est buckutt_trezo
-	 * 
-	 * @return int $state
-	 */
-	public function isBuckuttTrezo() {
-		if ($this->hasDroit("buckutt_trezo",0,0) == 1)
-			return 1;
-		else
-			return 454;
-	}	
-	
-	/**
-	 * Retourne si le user est point_admin
-	 * 
-	 * @return int $state
-	 */
-	public function isPointAdmin() {
-		if ($this->hasDroit("point_admin",0,0) == 1)
-			return 1;
-		else
-			return 454;
-	}
-
-	/**
-	 * Retourne si le user est resp_fundations
-	 * 
-	 * @return int $state
-	 */
-	public function isRespFundations() {
-		if ($this->hasDroit("resp_fundations",0,0) == 1)
-			return 1;
-		else
-			return 454;
-	}	
-	
-		/**
-	 * Retourne si le user est vente_admin
-	 * 
-	 * @param int $fun_id
-	 * @return int $state
-	 */
-	public function isVenteAdmin($fun_id) {
-		if ($this->hasDroit("vente_admin", $fun_id, 0) == 1)
-			return 1;
-		else
-			return 454;
-	}	
-	
-	/**
-	 * Retourne si le user est group_editor
-	 * 
-	 * @param int $fun_id
-	 * @return int $state
-	 */
-	public function isGroupEditor($fun_id) {
-		if ($this->hasDroit("group_editor", $fun_id, 0) == 1)
-			return 1;
-		else
-			return 454;
-	}	
-	
-	/**
-	 * Retourne si le user est fund_trezo
-	 * 
-	 * @param int $fun_id
-	 * @return int $state
-	 */
-	public function isFundTrezo($fun_id) {
-		if ($this->hasDroit("fund_trezo", $fun_id, 0) == 1)
-			return 1;
-		else
-			return 454;
-	}
-
-
 	/** 
 	*
 	*	Retourne si l'utilisateur est majeur
@@ -615,7 +337,7 @@ class User {
             $this->initGinger();
         }
         catch (Exception $ex) {
-            return 0;
+            throw new GingerFailure($ex);
         }
         
         return $this->gingerUser->is_adulte;
@@ -632,7 +354,7 @@ class User {
             $this->initGinger();
         }
         catch (Exception $ex) {
-            return 0;
+            throw new GingerFailure($ex);
         }
         
         return $this->gingerUser->is_cotisant;
@@ -644,7 +366,7 @@ class User {
 	* @return array $return
 	*/
 	public function getLastPurchase() {
-		$res = Db_buckutt::getInstance()->query("SELECT pur_id, obj_id, pur_price FROM t_purchase_pur WHERE UNIX_TIMESTAMP(pur_date) > (UNIX_TIMESTAMP(NOW()) - 900) AND usr_id_buyer = %u AND pur_removed = 0",array($this->idUser));
+		$res = $this->db->query("SELECT pur_id, obj_id, pur_price FROM t_purchase_pur WHERE UNIX_TIMESTAMP(pur_date) > (UNIX_TIMESTAMP(NOW()) - 900) AND usr_id_buyer = %u AND pur_removed = 0", array($this->idUser));
 		$pur = array();
 		while ($don = Db_buckutt::getInstance()->fetchArray($res)) { $pur[$don['pur_id']] = $don;}
 		return $pur;
@@ -727,5 +449,39 @@ class User {
 		$this::decCreditById($this->getId(), $val);
 		$this->credit -= $val;
 	}
-}
 
+    public static function getUserFromCas($ticket, $service) {
+        // Récupération du login via le CAS
+		//$login = Cas::authenticate($ticket, $service);
+        $login = "puyouart";
+		if ($login < 0) {
+			throw new CasFailure("Impossible de valider le ticket CAS fourni");
+		}
+        
+        // Création de l'objet user
+		$user = new User($login, 0, 0);
+        
+        return $user;
+    }
+    
+    public static function getUserFromBadge($badge) {
+		$ginger = new Ginger($_CONFIG['ginger_key']);
+		try {
+			$gingerUser = $ginger->getCard($badge_id);
+		}
+		catch (Exception $ex) {
+            throw new GingerFailure($ex);
+		}
+        
+        if(!$gingerUser->login) {
+            throw new UserUnknown();
+        }
+
+        $user = new User($gingerUser->login, 0, 0);
+
+        // On a déjà un résultat de ginger récent, donc autant le garder
+        $user->setGingerUser($gingerUser);
+            
+        return $user;
+    }
+}
