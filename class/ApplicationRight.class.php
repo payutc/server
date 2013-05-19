0cc712a1c17a60290dbb46637fc2b07ab0838fde
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2013-05-19 17:27:11 +0200

    correct indentation

--- class/ApplicationRight.class.php
+++ class/ApplicationRight.class.php
@@ -19,7 +19,7 @@ class ApplicationRight {
      * Verifie un tuple de droits.
      * Lorsque les droits n'existe pas throw an exception
      */
-	public static function check($application_id, $service_name = false, $check_fundation = false, $fundation_id = NULL) {
+    public static function check($application_id, $service_name = false, $check_fundation = false, $fundation_id = NULL) {
         $db = Db_buckutt::getInstance();
         $req = "SELECT afu.afu_id FROM tj_app_fun_afu afu 
                             WHERE afu.app_id = '%u' 
@@ -36,12 +36,12 @@ class ApplicationRight {
             $res = $db->query($req, array($application_id, $service_name));
         }
 
-		if ($db->affectedRows() == 0) {
+        if ($db->affectedRows() == 0) {
             if($fundation_id)
-	            throw new \Payutc\Exception\CheckRightException("L'application_id $application_id n'a pas les droits $service_name sur la fundation n°$fundation_id");
+                throw new \Payutc\Exception\CheckRightException("L'application_id $application_id n'a pas les droits $service_name sur la fundation n°$fundation_id");
             else
                 throw new \Payutc\Exception\CheckRightException("L'application_id $application_id n'a les droits $service_name sur aucune fundation");
-	    }
+        }
         return true;
     }
 
@@ -51,16 +51,16 @@ class ApplicationRight {
     public static function getFundations($application_id, $service_name) {
         $db = Db_buckutt::getInstance();
         $res = $db->query("SELECT fun.fun_id, fun.fun_name
-					FROM t_fundation_fun fun, tj_app_fun_afu afu
-					WHERE (afu.fun_id = fun.fun_id OR afu.fun_id is NULL)
+                    FROM t_fundation_fun fun, tj_app_fun_afu afu
+                    WHERE (afu.fun_id = fun.fun_id OR afu.fun_id is NULL)
                         AND afu.app_id = '%u'
                         AND (afu.afu_service = '%s' OR afu.afu_service IS NULL)
                         AND afu.afu_removed is NULL;", array($application_id, $service_name));
         $fundations = array();
         if ($db->affectedRows() >= 1) {
-			while ($don = $db->fetchArray($res)) {
+            while ($don = $db->fetchArray($res)) {
                 $fundations[$don["fun_id"]] = $don["fun_name"]; 
-			}
+            }
         }
         return $fundations;
     }
@@ -72,19 +72,19 @@ class ApplicationRight {
         $db = Db_buckutt::getInstance();
         if($fun_id) {
             $res = $db->query("SELECT afu.afu_id, afu.app_id, afu.fun_id, afu.afu_service
-					    FROM tj_app_fun_afu afu
-					    WHERE afu.fun_id = '%u'
+                        FROM tj_app_fun_afu afu
+                        WHERE afu.fun_id = '%u'
                             AND afu.afu_removed is NULL;", array($fun_id));
         } else {
             $res = $db->query("SELECT afu.afu_id, afu.app_id, afu.fun_id, afu.afu_service
-					    FROM tj_app_fun_afu afu
-					    WHERE afu.fun_id IS NULL
+                        FROM tj_app_fun_afu afu
+                        WHERE afu.fun_id IS NULL
                             AND afu.afu_removed is NULL;", array());
         }
 
         $rights = array();
         if ($db->affectedRows() >= 1) {
-			while ($don = $db->fetchArray($res)) {
+            while ($don = $db->fetchArray($res)) {
                 if(!array_key_exists($don["app_id"], $rights))
                 {
                     $rights[$don["app_id"]] = array();
@@ -93,11 +93,11 @@ class ApplicationRight {
                     $rights[$don["app_id"]]["service"] = array();
                 } 
                 $rights[$don["app_id"]]["service"][] = array("service" => $don["afu_service"], "id" => $don["afu_id"]);
-			}
+            }
         }
         return $rights;
     }
-	
+    
     /**
      * Donne les droits à une application sur un service et une fundation
      */
@@ -129,16 +129,16 @@ class ApplicationRight {
 
         $db->query($query_start . $query_end . ");", $var);
         if ($db->affectedRows() != 1) {
-			throw new Exception("Une erreur s'est produite lors de l'ajout du droit.");
-		}
+            throw new Exception("Une erreur s'est produite lors de l'ajout du droit.");
+        }
         return $db->insertId();
     }
 
-	/**
-	* Supprimer le droit
-	* 
-	*/
-	public static function removeRight($app_id, $service, $fun_id) {
+    /**
+    * Supprimer le droit
+    * 
+    */
+    public static function removeRight($app_id, $service, $fun_id) {
         $db = Db_buckutt::getInstance();
         $query = "UPDATE tj_app_fun_afu SET afu_removed=NOW() WHERE app_id='%u' ";
         $var = array($app_id);
@@ -158,9 +158,9 @@ class ApplicationRight {
         }  
 
         $db->query($query, $var);
-		if ($db->affectedRows() == 0) {
-			throw new Exception("Une erreur s'est produite lors de la supression du droit.");
-		}	
-	}
+        if ($db->affectedRows() == 0) {
+            throw new Exception("Une erreur s'est produite lors de la supression du droit.");
+        }    
+    }
 }
 
