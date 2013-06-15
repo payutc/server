c6d77d5eeb3526d8f1e915dc671fec4fcefc9f5c
Matthieu Guffroy <mattgu74@gmail.com>
2013-06-15 22:52:55 +0200

    Ecriture de tests unitaires pour plusieurs fonction du Bom Product

--- tests/ProductRwdbTest.php
+++ tests/ProductRwdbTest.php
@@ -31,6 +31,43 @@ class ProductRwdbTest extends DatabaseTest
         $r = Product::getOne(1, 1);
         $this->assertEquals(14, $r['stock']);
     }
+
+    public function testAdd() {
+        $a = Product::add("Chouffe", 1000, 180, 10, 1, null, 1);
+        $r = Product::getOne($a["success"], 1);
+
+        $this->assertEquals("Chouffe", $r['name']);
+        $this->assertEquals(10, $r['stock']);
+        $this->assertEquals(180, $r['price']);
+        $this->assertEquals(1, $r['alcool']);
+        $this->assertEquals(null, $r['image']);
+        $this->assertEquals(1000, $r['categorie_id']);
+        $this->assertEquals(1, $r['fundation_id']);
+        
+        $a = Product::edit($r["id"], "MC Chouffe", 1001, 170, 10, 0, null, 1);
+        $r = Product::getOne($a["success"]);
+
+        $this->assertEquals("MC Chouffe", $r['name']);
+        $this->assertEquals(10, $r['stock']);
+        $this->assertEquals(170, $r['price']);
+        $this->assertEquals(0, $r['alcool']);
+        $this->assertEquals(null, $r['image']);
+        $this->assertEquals(1001, $r['categorie_id']);
+        $this->assertEquals(1, $r['fundation_id']);
+
+        $a = Product::delete($r["id"], 1);
+        $z = Product::getOne($r["id"]);
+        $this->assertEquals(null, $z);
+
+        $r = Product::getOne($r["id"], 1, 1);
+        $this->assertEquals("MC Chouffe", $r['name']);
+        $this->assertEquals(10, $r['stock']);
+        $this->assertEquals(170, $r['price']);
+        $this->assertEquals(0, $r['alcool']);
+        $this->assertEquals(null, $r['image']);
+        $this->assertEquals(1001, $r['categorie_id']);
+        $this->assertEquals(1, $r['fundation_id']);
+    }
 }
 
 
