6f5169e56c8bb0b5a50815f76e3d6c65d5c1bcf1
Matthieu Guffroy <mattgu74@gmail.com>
2013-06-18 23:16:02 +0200

    Fix the key test

--- tests/Payutc/Service/KeyRwdbTest.php
+++ tests/Payutc/Service/KeyRwdbTest.php
@@ -28,17 +28,21 @@ class KeyRwdbTest extends DatabaseTest
             'service' => 'KEY'
         ));
         $this->assertEquals(200, $r->code);
+
         $r = httpSend('KEY', 'getCurrentUserApplications', $cookie, array());
         $this->assertEquals(200, $r->code);
         $nb_apps = count($r->body);
-        $apps = $r->body;
+
         $r = httpSend('KEY', 'registerApplication', $cookie, array(
             'app_url' => "https://localhost/",
             'app_name' => "Test app",
             'app_desc' => "App desc"
         ));
-        $this->assertEquals($nb_apps + 1, count($r->body));
         $this->assertEquals(200, $r->code);
+
+        $r = httpSend('KEY', 'getCurrentUserApplications', $cookie, array());
+        $this->assertEquals(200, $r->code);
+        $this->assertEquals($nb_apps + 1, count($r->body));
     }
 }
 
