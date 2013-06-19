44e63e3b35d6f1a9325042feee251b20322b0048
Matthieu Guffroy <mattgu74@gmail.com>
2013-06-19 22:03:04 +0200

    Test app_lastuse + loginApp

--- tests/Payutc/Service/KeyRwdbTest.php
+++ tests/Payutc/Service/KeyRwdbTest.php
@@ -39,10 +39,39 @@ class KeyRwdbTest extends DatabaseTest
             'app_desc' => "App desc"
         ));
         $this->assertEquals(200, $r->code);
+        $this->assertEquals("https://localhost/", $r->body['app_url']);
+        $this->assertEquals("Test app", $r->body['app_name']);
+        $this->assertEquals("App desc", $r->body['app_desc']);
+        $this->assertEquals("trecouvr", $r->body['app_creator']);
+        $id = $r->body['app_id'];
+        $key = $r->body['app_key'];
 
         $r = httpSend('KEY', 'getCurrentUserApplications', $cookie, array());
         $this->assertEquals(200, $r->code);
         $this->assertEquals($nb_apps + 1, count($r->body));
+        $app_lastuse = null;
+        foreach($r->body as $app) {
+            if($app['app_id'] == $id) {
+                $app_lastuse = $app['app_lastuse'];
+            }
+        }
+        $this->assertTrue($app_lastuse !== null);
+
+        $r = httpSend('KEY', 'loginApp', $cookie, array(
+            'key' => $key
+        ));
+        $this->assertEquals(200, $r->code);
+        $this->assertEquals(true, $r->body);
+
+        $r = httpSend('KEY', 'getCurrentUserApplications', $cookie, array());
+        $this->assertEquals(200, $r->code);
+        foreach($r->body as $app) {
+            if($app['app_id'] == $id) {
+                $this->assertTrue($app_lastuse !=  $app['app_lastuse']);
+            }
+        }
+              
+        
     }
 }
 
