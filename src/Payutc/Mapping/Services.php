699d879bb2b7cbedfa3a1db15cac2c96114de57b
alecalve <antoine@alc.io>
2013-09-23 13:00:23 +0200

    Ajout au mapping

--- src/Payutc/Mapping/Services.php
+++ src/Payutc/Mapping/Services.php
@@ -13,21 +13,22 @@ class Services {
         'KEY',
         'ADMINRIGHT',
         'BLOCKED',
-        'GESARTICLE'
+        'GESARTICLE',
+        'MESSAGE'
     );
-    
+
     public static function get($name) {
         static::checkExist($name);
         $name = "Payutc\\Service\\$name";
         return new $name();
     }
-    
+
     public static function checkExist($name) {
         if (!in_array($name, static::$services)) {
             throw new \Payutc\Exception\ServiceNotFound("Service $name does not exist");
         }
     }
-    
+
     public static function getServices() {
         return static::$services;
     }
