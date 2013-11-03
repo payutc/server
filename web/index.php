f44d4febf99a8d1bc23ba770807686ddb1929e02
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2013-11-03 10:23:51 -0500

    Removed useless parameter

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require_once '../vendor/autoload.php';
 
 require_once 'config.inc.php';
 
-$app = \Payutc\WebApp::createApplication($_CONFIG, (isset($_HOST_PREFIX) ? $_HOST_PREFIX : null));
+$app = \Payutc\WebApp::createApplication($_CONFIG);
 
 // run app
 $app->run();
