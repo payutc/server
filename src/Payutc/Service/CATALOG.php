7a36173f4ed8c4b1a7f03fb5bd0d581e4cc20750
Hugo RODDE <rodde.hugo@gmail.com>
2013-12-05 15:20:57 +0100

    Ajout du service catalog

---
+++ src/Payutc/Service/CATALOG.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Payutc\Service;
+
+/**
+ * CATALOG.php
+ * 
+ * Ce service permet de récupérer le catalogue des produits (bieres, snacks, softs)
+ *
+ */
+class CATALOG extends \ServiceBase {
+    
+}
