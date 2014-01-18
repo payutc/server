96e687fa5245932cf6f1d2437174c882d3da02a0
Florent Thévenet <florent@fthevenet.fr>
2014-01-18 22:59:33 +0100

    Proof Of Concept : listing des méthodes disponibles sur un service
    Le parsing du docstring est à améliorer, là ça enlève purement et simplement tous les *

--- class/ServiceBase.class.php
+++ class/ServiceBase.class.php
@@ -414,9 +414,58 @@ class ServiceBase {
             return $session[$key];
         }
     } 
+
+    /**
+     * Renvoie la liste des méthodes utilisables sur ce service
+     */
+    public function getMethods() {
+        $excluded_methods = array('__construct', '__wakeup');
+
+        $r = new \ReflectionClass($this);
+        $methods = $r->getMethods(\ReflectionMethod::IS_PUBLIC);
+        $res = array();
+
+        foreach ($methods as $method) {
+            if ($method->isStatic() || in_array($method->getName(), $excluded_methods)) {
+                continue;
+            }
+
+            // On récupère le docstrinf de la fonction
+            $doc = $method->getDocComment();
+            if ($doc === false) {
+                // si il n'y a pas de doc on renvoie juste une chaine vide
+                $doc = "";
+            } else {
+                // on enlève le /** du début et le */ de la fin et les * au milieu (un peu violent peut être)
+                $doc = str_replace(array("/**", "*/", "*"), array('', '', ""), $doc);
+                $a = explode("\n", $doc);
+                // on enlève les espaces de début et fin
+                $a = array_map('trim', $a);
+                // comme ça on peut enlever les lignes vides
+                $a = array_filter($a, function($s) { return $s !== ""; });
+                // et on recombine le tout
+                $doc = implode("\n", $a);
+            }
+
+            // on récupère le nom de la fonction et les paramètres
+            $m = array('name' => $method->getName(),
+                       'comment' => $doc,
+                       'parameters' => array());
+
+            foreach ($method->getParameters() as $param) {
+                // pour chaque méthode on récupère le nom et la valeur
+                // par défaut si le paramètre est optionel
+                $p = array('name' => $param->getName());
+                if ($param->isOptional()) {
+                    $p['default'] = $param->getDefaultValue();
+                }
+                $m['parameters'][] = $p;
+            }
+
+            $res[] = $m;
+        }
+        return $res;
+    }
 }
-
-
-
 
 
