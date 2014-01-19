f026ffad432b569963a438c0e35c0e443a200fd7
Florent Thévenet <florent@fthevenet.fr>
2014-01-19 11:36:36 +0100

    Test unitaires pour getMethods et base introspectable pour faciliter les tests

--- class/ServiceBase.class.php
+++ class/ServiceBase.class.php
@@ -27,6 +27,7 @@ use \Payutc\Exception\UserError;
 use \Payutc\Bom\User;
 use \Payutc\Log;
 use \Payutc\Config;
+use \IntrospectableBase;
 
 /**
 * ServiceBase.class
@@ -39,7 +40,7 @@ use \Payutc\Config;
 
 use \Payutc\Db\DbBuckutt;
 
-class ServiceBase {
+class ServiceBase extends \IntrospectableBase {
     protected $db;
     protected $service_name;  // Nom du service
 	
@@ -413,61 +414,6 @@ class ServiceBase {
         else {
             return $session[$key];
         }
-    } 
-
-    /**
-     * Renvoie la liste des méthodes utilisables sur ce service
-     */
-    public function getMethods() {
-        $excluded_methods = array('__construct', '__wakeup');
-
-        $r = new \ReflectionClass($this);
-        $methods = $r->getMethods(\ReflectionMethod::IS_PUBLIC);
-        $res = array();
-
-        foreach ($methods as $method) {
-            if ($method->isStatic() || in_array($method->getName(), $excluded_methods)) {
-                continue;
-            }
-
-            // On récupère le docstrinf de la fonction
-            $doc = $method->getDocComment();
-            if ($doc === false) {
-                // si il n'y a pas de doc on renvoie juste une chaine vide
-                $doc = "";
-            } else {
-                $doc = str_replace( "\r", "\n", $doc);
-                $lines = explode("\n", $doc);
-                $clean_doc = "";
-
-                $nb_lines = count($lines);
-                for ($i = 1; $i < $nb_lines -1; $i++) {
-                    // on ignore la première ligne ( /** )
-                    // et la dernière ( */ )
-                    $clean_doc .= preg_replace('/^[\s\t]*\*[ ]?(.*)/i', '$1', $lines[$i])."\n";
-                }
-
-                $doc = substr($clean_doc, 0, -1);
-            }
-
-            // on récupère le nom de la fonction et les paramètres
-            $m = array('name' => $method->getName(),
-                       'comment' => $doc,
-                       'parameters' => array());
-
-            foreach ($method->getParameters() as $param) {
-                // pour chaque méthode on récupère le nom et la valeur
-                // par défaut si le paramètre est optionel
-                $p = array('name' => $param->getName());
-                if ($param->isOptional()) {
-                    $p['default'] = $param->getDefaultValue();
-                }
-                $m['parameters'][] = $p;
-            }
-
-            $res[] = $m;
-        }
-        return $res;
     }
 }
 
---
+++ tests/Payutc/Service/IntrospectableTest.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'utils.php';
+
+use \Payutc\Config;
+use \IntrospectableBase;
+
+class IntrospectionTestClass extends \IntrospectableBase {
+    public function __construct() {
+
+    }
+
+    public function __wakeup() {
+
+    }
+
+    /**
+     * Some documentation :
+     *  Very Indentation
+     */
+    public function coucou($a, $b=3) {
+
+    }
+
+    public static function instance() {
+
+    }
+}
+
+class IntrospectableTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @requires PHP 5.4
+     */
+    public function testGetmethods() {
+        $expected = array(array('name' => 'coucou',
+                                'comment' => "Some documentation :\n Very Indentation",
+                                'parameters' => array(array('name' => 'a'),
+                                                      array('name' => 'b',
+                                                            'default' => 3))),
+                          array('name' => 'getMethods',
+                                'comment' => 'Renvoie la liste des méthodes utilisables sur ce service',
+                                'parameters' => array()));
+        $c = new IntrospectionTestClass();
+        $this->assertEquals($expected, $c->getMethods());
+    }
+}
