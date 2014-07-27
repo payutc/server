60db5b4fd65bde59251e485fb39ffd1db1daff72
Thomas Recouvreux <thomas@recouvreux.com>
2014-07-27 19:50:55 +0200

    getImage64: Remove obsolete 'encode' parameter

--- class/ServiceBase.class.php
+++ class/ServiceBase.class.php
@@ -367,7 +367,7 @@ class ServiceBase {
 	 * @param int $outh Hauteur de l'image
 	 * @return array $csv
 	 */
-	public function getImage64($img_id, $outw = 0, $outh = 0, $encode=true) {
+	public function getImage64($img_id, $outw = 0, $outh = 0) {
         // A partir du moment ou l'on a les droits sur le service courant on peut récupérer les images        
         $this->checkRight();
 
@@ -409,22 +409,10 @@ class ServiceBase {
         // Redimensionnement
         imagecopyresampled($newgd, $oldgd, 0, 0, 0, 0, $outw, $outh, $width_orig, $height_orig);
 
-        // Récupération et encodage en base64
-        if($encode) {
-            ob_start();
-            imagepng($newgd);
-            $output = base64_encode(ob_get_contents());
-            ob_end_clean();
-        } else {
-            $seconds_to_cache = 36000000;
-            $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
-            header("Expires: $ts");
-            header("Pragma: cache");
-            header("Cache-Control: max-age=$seconds_to_cache");
-            header('Content-Type: image/png');
-            imagepng($newgd);
-            exit();
-        }
+        ob_start();
+        imagepng($newgd);
+        $output = base64_encode(ob_get_contents());
+        ob_end_clean();
 
         // Retour s'il y a une image correcte
         if($output != false){
