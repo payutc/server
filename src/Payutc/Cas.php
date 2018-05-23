796d165b453918f96e3ec93f2838c201e1bd962b
Antoine Giraud <AntoineGiraud@users.noreply.github.com>
2018-05-23 10:51:18 -0400

    Update Cas.php

--- src/Payutc/Cas.php
+++ src/Payutc/Cas.php
@@ -7,55 +7,45 @@ use \Httpful\Request;
 
 use \Payutc\Exception\AuthenticationFailure;
 
-class Cas
-{
+class Cas {
     protected $url;
     protected $timeout;
     
-    public function __construct($url, $timeout=10)
-    {
+    public function __construct($url, $timeout=10) {
         $this->url = $url;
         $this->timeout = $timeout;
     }
     
-    public function authenticate($ticket, $service)
-    {
+    public function authenticate($ticket, $service) {
         $r = Request::get($this->getValidateUrl($ticket, $service))
           ->sendsXml()
           ->timeoutIn($this->timeout)
           ->send();
         $user = trim(str_replace("\n", "", $r->raw_body));
-        /*
+        // Log::warning("AuthenticationFailure : ($ticket, $service)".$r->raw_body."\n".$r->body);
         try {
-            $xml = new SimpleXMLElement($r->body);
-        } catch (\Exception $e) {
-            throw new \UnexpectedValueException("Return cannot be parsed : '{$r->body}'");
-        }
-        
-        $namespaces = $xml->getNamespaces();
-        
-        $serviceResponse = $xml->children($namespaces['cas']);
-        $user = $serviceResponse->authenticationSuccess->user;
-        //*/ 
-        if ($user) {
-            return (string)$user; // cast simplexmlelement to string
-        } else { /*
+            $xml = new SimpleXMLElement(trim(str_replace("\n", "", $r->raw_body)));
+            $namespaces = $xml->getNamespaces();
+            $serviceResponse = $xml->children($namespaces['cas']);
+            $user = $serviceResponse->authenticationSuccess->user;
             $authFailed = $serviceResponse->authenticationFailure;
             if ($authFailed) {
                 $attributes = $authFailed->attributes();
                 Log::warning("AuthenticationFailure : ".$attributes['code']." ($ticket, $service)");
                 throw new AuthenticationFailure((string)$attributes['code']);
             }
-            else { //*/
-                Log::error("Cas return is weird : '{$user}'");
-                throw new \UnexpectedValueException($user);
-            // }
+        } catch (\Exception $e) {
+            $user = trim(str_replace("\n", "", $r->raw_body));
         }
-        // never reach there
+        if ($user) {
+            return (string)$user; // cast simplexmlelement to string
+        } else {
+                Log::error("\n Cas return is weird : '{$user}'\n");
+                throw new \UnexpectedValueException($user);
+        }
     }
     
-    public function getValidateUrl($ticket, $service)
-    {
+    public function getValidateUrl($ticket, $service) {
         return $this->url."serviceValidate?ticket=".urlencode($ticket)."&service=".urlencode($service);
     }
 }
